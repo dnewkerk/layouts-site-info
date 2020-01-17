bfc60e021c02fcc9cd94dc023ee62f99207e1dab
David Newkerk <keysplayer@gmail.com>
2020-01-16 19:38:01 -0600

    Update version.

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: layouts-site-info
 # about: Site info widget for use with Discourse Layouts (based on layouts-custom-html)
-# version: 0.1
+# version: 0.2
 # authors: David Newkerk
 
 enabled_site_setting :layouts_site_info_enabled
